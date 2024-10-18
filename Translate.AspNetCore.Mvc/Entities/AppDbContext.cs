06d8de21f4a194d0055119c34073de7a8a3fcd46
yusuf <yusuf@YUSUF>
2024-10-18 19:59:54 +0300

    veritabani eklendi

---
+++ Translate.AspNetCore.Mvc/Entities/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Translate.AspNetCore.Mvc.Models;
+
+namespace Translate.AspNetCore.Mvc.Entities
+{
+	public class AppDbContext: DbContext
+	{
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+		{
+
+		}
+
+		public DbSet<UserViewModel> UserViewModel { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+		}
+	}
+}
---
+++ Translate.AspNetCore.Mvc/Models/UserViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Translate.AspNetCore.Mvc.Models
+{
+	
+	public class UserViewModel
+	{
+		[Key]
+		public int Id { get; set; }	
+
+		[Required(ErrorMessage ="İSİM ALANI ZORUNLU")]
+		public string FirstName { get; set; }
+
+		[Required(ErrorMessage = "SOYİSİM ALANI ZORUNLU")]
+		public string LastName { get; set; }
+		[Required(ErrorMessage = "E-POSTA ALANI ZORUNLU")]
+		public string Email { get; set; }
+		[Required(ErrorMessage = "ŞİFRE ALANI ZORUNLU")]
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+	}
+}
