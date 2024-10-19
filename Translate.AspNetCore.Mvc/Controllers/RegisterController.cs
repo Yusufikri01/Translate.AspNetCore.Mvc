dbfa5febf95db090bd64f6278fd0ae0dae29f200
Muhammed Emre <Muhammed Emre@EMRE>
2024-10-19 14:41:10 +0300

    login ve register sayfa tasarımları gerçekleştirildi

---
+++ Translate.AspNetCore.Mvc/Controllers/RegisterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Translate.AspNetCore.Mvc.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Translate.AspNetCore.Mvc/Models/LoginModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Translate.AspNetCore.Mvc.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "E-POSTA ALANI ZORUNLU")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "ŞİFRE ALANI ZORUNLU")]
+        public string Password { get; set; }
+    }
+}
